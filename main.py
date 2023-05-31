d307d0ecc61ea45caba7106ecb95b5b3bd194038
thearyadev <personal@aryankothari.dev>
2023-05-31 19:17:40 -0400

    change api url

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import gspread
 import time
 import typing
 import requests
+import json
 
 RANDOM_CELL_LABEL: str = "A1"
 LISTING_ID_RANGE: str = "A3:A1000"
@@ -20,8 +21,10 @@ class CardListing(typing.NamedTuple):
 
 def get_tcgplayer_data(listing_id) -> CardListing:
     """gets data from tcgplayer"""
-    url: str = f"https://mpapi.tcgplayer.com/v2/product/{listing_id}/details"
-    data: dict = requests.get(url).json()  # gets data from url
+    url: str = f"https://mp-search-api.tcgplayer.com/v1/product/{listing_id}/details?mpfev=1680"
+    data: dict = requests.get(
+        url,
+    ).json() # gets data from url
     customAttrs: dict | None = data.get("customAttributes")  # check if key exists
 
     # creates CardListing object
@@ -32,7 +35,7 @@ def get_tcgplayer_data(listing_id) -> CardListing:
         cmc=customAttrs.get("convertedCost") if customAttrs else None,
         type=customAttrs.get("fullType") if customAttrs else None,
         price=data.get("marketPrice"),
-        url=f"https://www.tcgplayer.com/product/{listing_id}"
+        url=f"https://www.tcgplayer.com/product/{listing_id}",
     )
 
 
@@ -49,7 +52,9 @@ def write_row(sheet: gspread.Worksheet, cellRange: str, cardData: CardListing) -
     cardData = tuple(cardData)  # convert namedtuple to tuple
     for i, cell in enumerate(cells):
         # some filtering and assignment
-        cell.value = cardData[i] if not isinstance(cardData[i], list) else " ".join(cardData[i])
+        cell.value = (
+            cardData[i] if not isinstance(cardData[i], list) else " ".join(cardData[i])
+        )
 
     sheet.update_cells(cells)
     return None
@@ -58,7 +63,11 @@ def write_row(sheet: gspread.Worksheet, cellRange: str, cardData: CardListing) -
 def write_error(sheet: gspread.Worksheet, cell: gspread.Cell, message: str) -> None:
     """Writes an error message on the row that failed."""
     cell.value = f"ERROR: {message}"
-    sheet.update_cells([cell, ])
+    sheet.update_cells(
+        [
+            cell,
+        ]
+    )
 
 
 def write_status(sheet: gspread.Worksheet, status: str):
@@ -71,30 +80,34 @@ def main():
 
     randomCellOldValue: int = sheet.acell(RANDOM_CELL_LABEL).value
 
-    while True:  # loop checks if the user has requested a reload, this is done by a cell changing value.
+    while (
+        True
+    ):  # loop checks if the user has requested a reload, this is done by a cell changing value.
         try:
             newValueCheck: int = sheet.acell(RANDOM_CELL_LABEL).value
             if newValueCheck != randomCellOldValue:
                 randomCellOldValue = newValueCheck
                 write_status(sheet, status="Refreshing...")
-                for listing in (cell for cell in sheet.range(LISTING_ID_RANGE) if cell.value):
-                    try:
-                        write_row(
-                            sheet=sheet,
-                            cellRange=f"A{listing.row}:G{listing.row}",
-                            cardData=get_tcgplayer_data(listing.value)
-                        )
+                for listing in (
+                    cell for cell in sheet.range(LISTING_ID_RANGE) if cell.value
+                ):
+                    cdata = get_tcgplayer_data(listing.value)
 
-                    except Exception as e:
-                        write_error(sheet, sheet.cell(row=listing.row, col=2), message=str(e))
+                    write_row(
+                        sheet=sheet,
+                        cellRange=f"A{listing.row}:G{listing.row}",
+                        cardData=cdata,
+                    )
+
                     time.sleep(2)  # wait 2 seconds after each write
                 write_status(sheet, status="Waiting...")
             else:
                 write_status(sheet, status="Waiting...")
         except Exception as e:
             write_status(sheet, status=f"ERROR: {e}")
+            raise e
         time.sleep(10)  # wait 10 seconds before checking if a refresh was requested
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
